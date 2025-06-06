51d87112bb8071f3f4edd35c59f269507e1d2ad9
Raul Costa Junior <raulcostajunior@gmail.com>
2025-06-06 19:38:21 +0200

    Initial commit of the BoardWidget - no build method defined yet

---
+++ lib/board_widget.dart
@@ -0,0 +1,14 @@
+import 'package:fludoku/fludoku.dart';
+import 'package:flutter/widgets.dart';
+
+class BoardWidget extends StatelessWidget {
+  final Board _board;
+  const BoardWidget(this._board, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
