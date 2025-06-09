371eb86dbf2fb9a254bbcbf740cc629529058902
Raul Costa Junior <raulcostajunior@gmail.com>
2025-06-09 16:02:36 +0200

    Revert non necessary change to BoardProvider.updateShouldNotify

--- lib/state/board_provider.dart
+++ lib/state/board_provider.dart
@@ -19,9 +19,6 @@ class BoardProvider extends InheritedWidget {
 
   @override
   bool updateShouldNotify(covariant BoardProvider oldWidget) {
-    // The BoardProvider is, in practice, a singleton - it has the board, which
-    // is its most dynamic part, as one of its members. It notifies interested
-    // listener about changes in the board or of any of its state flags.
-    return false;
+    return _viewModel != oldWidget._viewModel;
   }
 }
